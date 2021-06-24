7511e0e6c42f39fe0290353959e049b9ac10b862
rayreveles <79915954+rayreveles@users.noreply.github.com>
2021-06-24 12:42:48 -0300

    Create Nota.java

---
+++ src/bo/Nota.java
@@ -0,0 +1,30 @@
+package bo;
+
+public class Nota {
+	private String codProva;
+
+	private String matricula;
+	private String nota;
+	
+	
+	public String getCodProva() {
+		return codProva;
+	}
+	public void setCodProva(String codProva) {
+		this.codProva = codProva;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getNota() {
+		return nota;
+	}
+	public void setNota(String nota) {
+		this.nota = nota;
+	}
+	
+	
+}
