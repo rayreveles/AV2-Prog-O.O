287854c9f9711825f6afc3c54485cb6dab9fecd6
rayreveles <79915954+rayreveles@users.noreply.github.com>
2021-06-24 12:43:09 -0300

    Create Professor.java

---
+++ src/bo/Professor.java
@@ -0,0 +1,23 @@
+package bo;
+
+public class Professor {
+	private String codProf;
+	private String nome;
+	public String getCodProf() {
+		return codProf;
+	}
+	public void setCodProf(String codProf) {
+		this.codProf = codProf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+
+}
+	
